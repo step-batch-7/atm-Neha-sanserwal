6608dda5e44d7efb69cf981a9342c2f3fef0b0a6
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2020-04-07 19:52:53 +0530

    added function display_notes

--- atm.c
+++ atm.c
@@ -4,6 +4,9 @@ unsigned int get_money(unsigned short int money){
   unsigned int count = 0;
   unsigned int domination[8] = {2000, 500, 100, 50, 20, 10, 5, 1};
   unsigned short int remaining = money;
+  if(remaining > 31999){
+    return count;
+  }
   int size = sizeof(domination)/sizeof(unsigned int);
   for(int i = 0;i < size;i++){
       count =  count << sizeof(unsigned int);
--- main.c
+++ main.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include "atm.h"
 
-int main(void)
-{
+int display_notes(count){
+  unsigned int dominations[8] = {1,5,10,20,50,100,500,2000},position = 0xf;
+  int num;
+  for(int i = 0; i < 8; i++){
+    num=count & position;
+    if(num){
+    printf("\n%u notes of %u",num,dominations[i]);
+    }
+    count = count >> 4;
+  }
+  return 0;
+}
+
+int main(void){
   unsigned short int amounts[] = {1, 10, 39, 589, 2590, 31999};
-  for (int i = 0; i < 6; i++)
-  {
+  for (int i = 0; i < 6; i++){
     unsigned int notes = get_money(amounts[i]);
     printf("%5u %08x\n", amounts[i], notes);
+    display_notes(notes);
   }
 
   return 0;
